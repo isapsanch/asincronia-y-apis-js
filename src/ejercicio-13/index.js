08e32fdd954482ca2ff9d5aedb94f9d42a6091e9
isapsanch <isabelpsanch@hotmail.com>
2023-11-12 14:05:20 +0100

    Añadir Ejercicio: 13

--- src/ejercicio-13/index.js
+++ src/ejercicio-13/index.js
@@ -1,1 +1,14 @@
 // Escribe tu código aquí:
+
+const axios = require('axios')
+
+async function obtenerDatosDeAPI() {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts/invalid')
+    console.log(response.data)
+  } catch (error) {
+    console.error('Error al obtener datos:', error.message)
+  }
+}
+
+obtenerDatosDeAPI()
